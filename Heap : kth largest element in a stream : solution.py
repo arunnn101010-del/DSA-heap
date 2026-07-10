a4a4929825d605c819db450ec1e4bedf28e53765
Arun <arunnn101010@gmail.com>
2026-07-10 23:19:03 +0530

    Update Heap : kth largest element in a stream : solution.py

--- Heap : kth largest element in a stream : solution.py
+++ Heap : kth largest element in a stream : solution.py
@@ -1,1 +1,29 @@
+# Promblem - kth largest element in a stream 
+# Time and space complexity - 0(N og k) & 0(k)
+# Leetcode and diffulty level - 703 & easy 
+class KthLargest {
+public:
+    priority_queue<int , vector<int>, greater<int>> pq;
+    int k;
 
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+
+        for(int num : nums) {
+            pq.push(num);
+
+            if(pq.size() > k ) {
+                pq.pop();
+            }
+        }    
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        
+        if(pq.size() > k) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
