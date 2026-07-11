3737df1fa5c2f9bd7dca7a498bdd4487c3584c0a
Arun <arunnn101010@gmail.com>
2026-07-11 22:56:45 +0530

    Update Heap : take gift from the richest pile : solution.py

--- Heap : take gift from the richest pile : solution.py
+++ Heap : take gift from the richest pile : solution.py
@@ -1,1 +1,31 @@
+# Promblem - take gift from the richest pile 
+# Appraoch - max heap 
+# Time and space complexity - 0(N + K(log N)) & 0(N) 
+# Leetcode and diffculty level - 2558 & easy
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        
+        priority_queue<int> pq;
 
+        for(int gift : gifts) {
+            pq.push(gift);
+        }
+
+        while(k--) {
+            int largest = pq.top();
+            pq.pop();
+
+            pq.push(sqrt(largest));
+        }
+
+        long long sum = 0;
+
+        while(!pq.empty()) {
+
+            sum += pq.top();
+            pq.pop();
+        }
+        return sum;
+    }
+};
