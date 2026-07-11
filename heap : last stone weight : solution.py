385cb8daf5acabdd5638a9529e795c90bc8ff754
Arun <arunnn101010@gmail.com>
2026-07-11 18:27:57 +0530

    Update heap : last stone weight : solution.py

--- heap : last stone weight : solution.py
+++ heap : last stone weight : solution.py
@@ -1,1 +1,31 @@
+# Promblem - last stone weight 
+# time and space complexity - 0(n log k) & 0(k) 
+# Leetcode and diffculty level - 1046 & easy 
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
 
+        priority_queue<int> pq;
+
+        for(int stone : stones) {
+            pq.push(stone);
+        }
+
+        while(pq.size() > 1) {
+            int first = pq.top();
+            pq.pop();
+
+            int second = pq.top();
+            pq.pop();
+
+            if(first != second) {
+                pq.push(first - second);
+            }
+        }
+
+        if(pq.empty()) {
+            return 0;
+        }
+        return pq.top();
+    }
+};
