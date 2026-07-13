e1a3cdc333531b4e71b3b6fc2353c15f6fe81b8c
Arun <arunnn101010@gmail.com>
2026-07-13 20:44:12 +0530

    Update Heap : top k frequent elements : solution.py

--- Heap : top k frequent elements : solution.py
+++ Heap : top k frequent elements : solution.py
@@ -1,1 +1,31 @@
+# promblem - top k frequent elements 
+# Approach - min heap 
+# Time and space complexity - 0(N log K ) & 0(n) 
+# Leetcode and diffculty level - 347 & medium 
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
 
+        for(int num : nums) {
+            mp[num]++;
+        }
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+        for(auto it : mp) {
+            pq.push({it.second, it.first});
+
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+        vector<int> ans;
+
+        while(!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
