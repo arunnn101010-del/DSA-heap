7bb6339447e167096c336d9de275c5d911c389ae
Arun <arunnn101010@gmail.com>
2026-07-13 20:07:26 +0530

    Update Heap : kth largest element in an array : solution.py

--- Heap : kth largest element in an array : solution.py
+++ Heap : kth largest element in an array : solution.py
@@ -1,1 +1,20 @@
+# Promblem - kth largest element in an array 
+# Approach - min heap 
+# Time and space complexity - 0( N log K ) & 0(k) 
+# Leetcode and diffculty level - 215 & medium 
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
 
+        for(int num : nums) {
+            pq.push(num);
+
+            if(pq.size() > k) {
+                pq.pop();
+
+            }
+        }
+        return pq.top();
+    }
+};
