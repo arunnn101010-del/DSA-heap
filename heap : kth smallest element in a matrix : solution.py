928ebeb0dbd4f577498a59bf53dc684577a3ffd4
Arun <arunnn101010@gmail.com>
2026-07-17 15:11:11 +0530

    Update heap : kth smallest element in a matrix : solution.py

--- heap : kth smallest element in a matrix : solution.py
+++ heap : kth smallest element in a matrix : solution.py
@@ -1,1 +1,25 @@
+# Promblem - kth smallest element in a matrix 
+# Approach - max heap 
+# Time and space complexity - 0(n log k) & 0(n)
+# Leetcode and diffculty level - 378 & medium 
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
 
+        priority_queue<int> pq;
+
+        for(auto &row : matrix){
+
+            for(int num : row){
+
+                pq.push(num);
+
+                if(pq.size() > k){
+                    pq.pop();
+                }
+            }
+        }
+
+        return pq.top();
+    }
+};
